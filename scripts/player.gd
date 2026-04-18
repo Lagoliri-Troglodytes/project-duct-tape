6b9833dfe8c3f0fc165b145f46ea8ab6936a472f
jbxsipeljk-pixel <jbxsipeljk@gmail.com>
2026-04-18 15:30:54 -0400

    Movement
    Player can move left and right

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,24 @@
 extends CharacterBody2D
 class_name Player
+
+@export var speed = 400
+var screen_size
+
+func ready():
+	screen_size = get_viewport_rect().size
+
+
 func _physics_process(delta: float) -> void:
-	#TODO: Get player input and change velocity
-	# then use the function move_and_slide()
-	pass
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
