355161c0e49da061e9ffb2f20a61fea3caa67b4a
Lagoliri-Troglodytes <wren.octothorpe@yahoo.com>
2026-04-18 15:34:02 -0400

    player camera now knows player velocity
    also main scene was correctly set to game again

--- scripts/player_camera.gd
+++ scripts/player_camera.gd
@@ -3,14 +3,26 @@ class_name PlayerCamera
 @onready var player : Player = self.get_node("../Player")
 
 var camera_speed : float = 12.0
+var camera_offset : Vector2 = Vector2(0.0,-0.125)
 func quick_teleport_to_player() -> void:
 	check_player_exists()
-	self.global_position = player.global_position
+	var screen_size : Vector2 = self.get_viewport_rect().size
+	self.global_position = player.global_position+camera_offset*screen_size
 func _process(delta: float) -> void:
 	check_player_exists()
+	var screen_size : Vector2 = self.get_viewport_rect().size
+	var player_vel_influence : Vector2 = player.get_real_velocity()*0.5
+	player_vel_influence.x = clamp(
+		player_vel_influence.x,
+		-screen_size.x*0.5,
+		screen_size.x*0.5)
+	player_vel_influence.y = clamp(
+		player_vel_influence.y,
+		-screen_size.y*0.5,
+		screen_size.y*0.5)
 	self.global_position = lerp(
 		self.global_position,
-		player.global_position,
+		player.global_position+player_vel_influence+camera_offset*screen_size,
 		clamp(camera_speed*delta,0.0,1.0))
 func check_player_exists():
 	assert((player is Player),"PlayerCamera can not find valid player!!")
