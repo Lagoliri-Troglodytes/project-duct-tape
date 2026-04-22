22cf8bf1a8b13f56095830716b88da90fdb04edf
Cheeziest0 <cheeziest0@gmail.com>
2026-04-21 20:28:22 -0400

    gravity increase + main menu work
    changed gravity to 1500 (3 blocks high) and got the main menu to work

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,9 +3,35 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_button_pressed() -> void:
+	$MainMenuBackground.hide()
+	$DuctTapeSprite.hide()
+	$CreditsBackground.hide()
+	$Title.hide()
+	$StartButton.hide()
+	$CreditsButton.hide()
+	$CreditsCloseButton.hide()
+	$CreditsList.hide()
+
+
+func _on_credits_button_pressed() -> void:
+	$Title.text = "Credits"
+	$CreditsBackground.show()
+	$CreditsCloseButton.show()
+	$CreditsList.show()
+	pass # show CreditsBackground, CreditsCloseButton and CreditsList
+
+
+func _on_credits_close_button_pressed() -> void:
+	$Title.text = "Project Duct Tape"
+	$CreditsBackground.hide()
+	$CreditsCloseButton.hide()
+	$CreditsList.hide()
