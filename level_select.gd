d5f274a939c40045735beed2a4edb03704e40ada
Cheeziest0 <cheeziest0@gmail.com>
2026-04-30 21:39:28 -0400

    keyboard/controller ui navigation
    added keyboard ui navigation (wasd/arrow keys, space/enter) and controller ui navigation (sticks/dpad, every face button because i'm not sure which one is correct)

--- level_select.gd
+++ level_select.gd
@@ -2,7 +2,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Level1Button.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,13 +3,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$StartButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_start_button_pressed() -> void:
 	$CreditsBackground.hide()
@@ -24,6 +23,7 @@ func _on_credits_button_pressed() -> void:
 	$CreditsCloseButton.show()
 	$CreditsTitle.show()
 	$CreditsList.show()
+	$CreditsCloseButton.grab_focus()
 
 func _on_credits_close_button_pressed() -> void:
 	$Title.show()
@@ -31,3 +31,4 @@ func _on_credits_close_button_pressed() -> void:
 	$CreditsCloseButton.hide()
 	$CreditsTitle.hide()
 	$CreditsList.hide()
+	$CreditsButton.grab_focus()
